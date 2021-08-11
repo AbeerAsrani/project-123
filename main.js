6475919c821c3744e7a44dc124c5d2cd2e013c53
AbeerAsrani <72503306+AbeerAsrani@users.noreply.github.com>
2021-08-11 16:50:59 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,49 @@
+
+noseX=0;
+noseY=0;
+difference=0;
+left_wrist_x=0;
+right_wrist_x=0;
+
+function setup()
+{
+video=createCapture(VIDEO);
+video.size(500,500);
+canvas=createCanvas(500,500);
+canvas.position(580,200);
+poseNet=ml5.poseNet(video,modelLoaded);
+poseNet.on('pose',gotPoses);
+}
+
+function preload()
+{
+
+}
+
+function draw()
+{
+background("blue");
+fill("#34c0eb");
+stroke("white");
+textSize(difference)
+text('Abeer', noseX,noseY);
+document.getElementById("font_size").innerHTML="Width and Height of the font is "+difference+"px";
+}
+function modelLoaded()
+{
+    console.log("model is loaded");
+}
+function gotPoses(results)
+{
+if(results.length>0){
+    console.log(results);
+    noseX=results[0].pose.nose.x;
+    noseY=results[0].pose.nose.y;
+    left_wrist_x=results[0].pose.leftWrist.x;
+    right_wrist_x=results[0].pose.rightWrist.x;
+    difference=floor(left_wrist_x-right_wrist_x)
+}
+    
+
+
+}
